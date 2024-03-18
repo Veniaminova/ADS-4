896534c33dbc771f66a5b7b5a83f04054eeaed6f
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-03-18 20:26:09 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,7 @@ int countPairs1(int *arr, int len, int value) {
     int summa = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
-            if(value == arr[i] + arr[j]){
+            if(value == arr[i] + arr[j]) {
                 summa += 1;
             }
         }
@@ -18,40 +18,36 @@ int countPairs2(int *arr, int len, int value) {
     int summa = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = len-1; j > i; --j) {
-            if(value == arr[i] + arr[j]){
+            if(value == arr[i] + arr[j]) {
                 summa += 1;
             }
         }
     }
     return summa;
 }
-int countPairs3(int *arr, int len, int value) {
+int binarySearch(int* arr, int left, int right, int key) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == key) {
+        return mid;
+        }
+        if (arr[mid] < key) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int countPairs3(int* arr, int len, int value) {
     int summa = 0;
-    for (int i = 0; i < len - 1; i++) {
-        int leftInd = i;
-	int rightInd = len;
-	while (leftInd < rightInd - 1) {
-	    int middle = (leftInd + rightInd) / 2;
-	    if (arr[i] + arr[middle] == value) {
-	        summa = summa + 1;
-	        int j = middle + 1;
-                while (arr[i] + arr[j] == value && j < rightInd) {
-		    summa = summa + 1;
-		    j = j + 1;
-		}
-		j = middle - 1;
-                while (arr[i] + arr[j] == value && j > leftInd) {
-                    summa = summa + 1;
-                    j = j - 1;
-                }
-                break;
-            }
-	    if (arr[i] + arr[middle] > value) {
-                rightInd = middle;
-            }
-            else {
-                leftInd = middle;
-            }
+    for (int i = 0; i < len; ++i) {
+        int secondElement = value - arr[i];
+        int index = binarySearch(arr, i + 1, len - 1, secondElement);
+        if (index != -1) {
+            summa++;
         }
     }
     return summa;
