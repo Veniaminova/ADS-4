f529ac2cfa9b014db525bf490f466ae98600b607
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-03-19 18:48:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,52 +5,52 @@ int countPairs1(int *arr, int len, int value) {
     int summa = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
-            if(value == arr[i] + arr[j]) {
+            if (value == arr[i] + arr[j]) {
                 summa = summa + 1;
             }
         }
     }
     return summa;
 }
+
 int countPairs2(int *arr, int len, int value) {
     int summa = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = len-1; j > i; --j) {
-            if(value == arr[i] + arr[j]) {
+            if (value == arr[i] + arr[j]) {
                 summa = summa + 1;
             }
         }
     }
     return summa;
 }
-int countPairs3(int* arr, int len, int value){
-    int summa = 0;
+
+int countPairs3(int* arr, int len, int value) {
+    int result = 0;
     for (int i = 0; i < len - 1; i++) {
-        int leftInd = i;
-        int rightInd = len;
-        while (leftInd < rightInd - 1) {
-            int middle = (leftInd + rightInd) / 2;
-            if (arr[i] + arr[middle] == value) {
-                summa = summa + 1;
-                int j = middle + 1;
-                while (arr[i] + arr[j] == value && j < rightInd) {
-                    summa = summa + 1;
-                    j = j + 1;
-                }
-                j = middle - 1;
-                while (arr[i] + arr[j] == value && j > leftInd) {
-                    summa = summa + 1;
-                    j = j - 1;
-                }
-                break;
+    int l_bound = i;
+    int r_bound = len;
+    while (l_bound < r_bound - 1) {
+        int middle = (r_bound + l_bound) / 2;
+        if (arr[i] + arr[middle] == value) {
+            result++;
+            int j = middle + 1;
+            while ((arr[i] + arr[j] == value) && (j < r_bound)) {
+                result++;
+                j++;
             }
-            if (arr[i] + arr[middle] > value) {
-                rightInd = middle;
+            j = middle - 1;
+            while ((arr[i] + arr[j] == value) && (j > l_bound)) {
+                result++;
+                j--;
             }
-            else {
-                leftInd = middle;
+            break;
+            } else if (arr[i] + arr[middle] > value) {
+                r_bound = middle;
+            } else {
+                l_bound = middle;
             }
         }
     }
-    return summa;
+    return result;
 }
